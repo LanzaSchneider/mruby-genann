86a0c1a1f8eb62f8b076504124fb24633cdde1e1
LanzaSchneider <lanzaschneider@conchship.net>
2023-04-17 16:49:23 +0800

    feat: update README.md and test cases

--- mrbgem.rake
+++ mrbgem.rake
@@ -3,4 +3,5 @@ MRuby::Gem::Specification.new('mruby-genann') do |spec|
   spec.authors = 'Lanza Schneider'
   spec.add_test_dependency('mruby-pack', :core => 'mruby-pack')
   spec.add_test_dependency('mruby-sprintf', :core => 'mruby-sprintf')
+  spec.add_test_dependency('mruby-print', :core => 'mruby-print')
 end
--- test/genann.rb
+++ test/genann.rb
@@ -5,13 +5,13 @@ assert 'example1' do
   output_buf = output.collect {|arr| arr.pack('E*') }
   ann = Genann.new(2, 1, 2, 1)
   500.times do
-    ann.train input_buf[0], output_buf[0], 3
-    ann.train input_buf[1], output_buf[1], 3
-    ann.train input_buf[2], output_buf[2], 3
-    ann.train input_buf[3], output_buf[3], 3
+    ann.train input_buf[0], output_buf[0], 1.0
+    ann.train input_buf[1], output_buf[1], 1.0
+    ann.train input_buf[2], output_buf[2], 1.0
+    ann.train input_buf[3], output_buf[3], 1.0
   end
-  assert_equal output[0][0].to_i, ann.run(input_buf[0]).unpack('E*')[0].round
-  assert_equal output[1][0].to_i, ann.run(input_buf[1]).unpack('E*')[0].round
-  assert_equal output[2][0].to_i, ann.run(input_buf[2]).unpack('E*')[0].round
-  assert_equal output[3][0].to_i, ann.run(input_buf[3]).unpack('E*')[0].round
+  puts sprintf('Output for [%1.f, %1.f] is %1.f.', input[0][0], input[0][1], ann.run(input_buf[0]).unpack('E*')[0])
+  puts sprintf('Output for [%1.f, %1.f] is %1.f.', input[1][0], input[1][1], ann.run(input_buf[1]).unpack('E*')[0])
+  puts sprintf('Output for [%1.f, %1.f] is %1.f.', input[2][0], input[2][1], ann.run(input_buf[2]).unpack('E*')[0])
+  puts sprintf('Output for [%1.f, %1.f] is %1.f.', input[3][0], input[3][1], ann.run(input_buf[3]).unpack('E*')[0])
 end
